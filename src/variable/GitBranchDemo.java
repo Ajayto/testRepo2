b413aff2e2dfc73440af4b096301fea1854048f5
unknown <ajaytoradmal2009@gmail.com>
2024-02-03 11:41:27 +0530

    updated message for gitdemo.java

--- src/variable/GitBranchDemo.java
+++ src/variable/GitBranchDemo.java
@@ -5,7 +5,7 @@ public class GitBranchDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("This is to demo git branch");
+		System.out.println("this is ScriptQA batch 70 and we are leraning git branch");
 
 	}
 
