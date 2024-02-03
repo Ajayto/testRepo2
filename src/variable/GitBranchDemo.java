6cd08891c4279b761c07d999386fe97d0e24f9f4
unknown <ajaytoradmal2009@gmail.com>
2024-02-03 11:16:37 +0530

    created program to demo git branch

---
+++ src/variable/GitBranchDemo.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class GitBranchDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is to demo git branch");
+
+	}
+
+}
