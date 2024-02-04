b296cdc44f543ee3a68b0dcdc114b7ce15028d9e
Ajayto <ajaytoradmal2009@gmail.com>
2024-02-04 10:22:14 +0530

    Update WhileLoop.java

--- src/variable/WhileLoop.java
+++ src/variable/WhileLoop.java
@@ -4,8 +4,8 @@ public class WhileLoop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		int i=12;
+		//updated value of I to 0
+		int i=0;
 		while(i<11) {
 			System.out.println(i);
 			i++;
